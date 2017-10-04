99c14853f882653fc835c4302d10a901b3169121
Fernanda <maryfer3112@hotmail.com>
2017-10-04 04:08:25 +0200

    botonsito ue habia olvidadoooo aaaaaaaaaaa

--- PracticaIntents/app/src/main/java/com/example/invitado/practicaintents/Principal.java
+++ PracticaIntents/app/src/main/java/com/example/invitado/practicaintents/Principal.java
@@ -6,10 +6,11 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Principal extends AppCompatActivity {
+public class Principal extends AppCompatActivity implements OnClickListener {
 
     SharedPreferences shared = null;
 
@@ -17,6 +18,9 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        View btn = findViewById(R.id.btn_cambiar);
+        btn.setOnClickListener(this);
 
         shared = getSharedPreferences("firstrun", MODE_PRIVATE);
 
@@ -36,4 +40,10 @@ public class Principal extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        Intent abrir = new Intent(this, Actividad2.class);
+        startActivity(abrir);
+        Toast.makeText(this, "Cambiando a actividad 2", Toast.LENGTH_LONG).show();
+    }
 }
