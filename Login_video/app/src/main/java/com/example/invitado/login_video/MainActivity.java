82a064f327d5761534fc5d050dcdd833f22252eb
Fernanda <maryfer3112@hotmail.com>
2017-11-09 17:30:08 +0100

    login con video

---
+++ Login_video/app/src/main/java/com/example/invitado/login_video/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.invitado.login_video;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+       VideoView vv = (VideoView)findViewById(R.id.vv_video);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.bg_video);
+        vv.setVideoURI(uri);
+        vv.start();
+
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+    }
+
+
+}
